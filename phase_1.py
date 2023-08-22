f221eb9fe52e8dd64541146e0f96a30cc1f066b4
marzeverett <marzeverett@gmail.com>
2023-08-22 08:20:38 -0700

    Fix phase 1

--- phase_1.py
+++ phase_1.py
@@ -1,19 +1,9 @@
 import json 
 import run_experiments
 
-letters = ['A', 'B', 'C', 'E', 'F', 'G', 'L', 
-    'M', 'S', 'T', 'AC', 'AD']
 
 #Actual letters we will use!!! 
-#letters = ['A', 'C', 'E', 'F', 'G', 'S', 'T', 'AC', 'AD']
-
-#letters = ['E', 'F', 'G', 'S', 'T', 'AC', 'AD']
-
-#letters = ['A', 'B', 'C', 'E']
-
-#letters = ['A']
-
-letters = ['S', 'T', 'AC', 'AD']
+letters = ['A', 'C', 'E', 'F', 'G', 'S', 'T', 'AC', 'AD']
 
 phase_dict = {
     "phase_name": "1",

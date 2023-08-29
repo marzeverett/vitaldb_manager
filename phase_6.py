4e257b55b08f3f333777d0620ef89363c993dbf2
marzeverett <marzeverett@gmail.com>
2023-08-29 10:55:11 -0700

    Fixed mistake in phase 6 definition

--- phase_6.py
+++ phase_6.py
@@ -8,7 +8,7 @@ letters = ['A', 'C', 'E', 'F', 'G', 'S', 'T', 'AC', 'AD']
 phase_dict = {
     "phase_name": "6",
     "phase_path_start": "generated_files/",
-    "delete_stream": "orch",
+    "delete_stream": ["orch"],
     "task_type": "regression",
     "model_group": "0",
     "input_samples": [30],
